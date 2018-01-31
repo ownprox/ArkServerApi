30f39fa5e228a0eb51e267e461743905a9cf5967
Michidu <michido@protonmail.com>
2018-01-31 19:34:50 +0300

    Added configurable messages Added distance check Replaced server messages Fixed crash /atrack now takes player or tribe id Dinos can be searched by tag now Some other improvements

--- DinoTracker/DinoTracker/DinoTracker.h
+++ DinoTracker/DinoTracker/DinoTracker.h
@@ -1,23 +1,8 @@
 #pragma once
-#include <Windows.h>
-#include "API\ARK\Ark.h"
-bool PlayerCanTrack;
-std::wstring PlayerTrackDinoSyntaxError, PlayerTrackDinoSyntaxError1;
-std::string utf8_encode(const std::wstring &wstr)
-{
-	if (wstr.empty()) return std::string();
-	int size = wstr.length() + 1;
-	std::string strTo(size, 0);
-	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size, NULL, NULL);
-	return strTo;
-}
 
-// Convert an UTF8 string to a wide Unicode String
-std::wstring utf8_decode(const std::string &str)
-{
-	if (str.empty()) return std::wstring();
-	int size = str.length() + 1;
-	std::wstring wstrTo(size, 0);
-	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size);
-	return wstrTo;
-}
+#include <Windows.h>
+#include <API/ARK/Ark.h>
+
+inline bool PlayerCanTrack;
+inline float MinDistance;
+inline std::wstring Messages[10];
--- DinoTracker/DinoTracker/DinoTrackerConfig.h
+++ DinoTracker/DinoTracker/DinoTrackerConfig.h
@@ -1,17 +1,41 @@
 #pragma once
+
+#include "DinoTracker.h"
+
 #include <fstream>
 #include "json.hpp"
 
-void InitConfig()
+inline void InitConfig()
 {
 	std::ifstream file(ArkApi::Tools::GetCurrentDir() + "/ArkApi/Plugins/DinoTracker/config.json");
 	if (!file.is_open())
 	{
-		PlayerCanTrack = true;
-		return;
+		Log::GetLog()->error("Failed to load config.json");
+		throw;
 	}
+
 	nlohmann::json Config;
-	file >> Config;
+
+	try
+	{
+		file >> Config;
+	}
+	catch (const std::exception& error)
+	{
+		Log::GetLog()->error(error.what());
+		throw;
+	}
+
 	file.close();
+
 	PlayerCanTrack = Config["DinoTracker"]["PlayersCanTrack"];
+	MinDistance = Config["DinoTracker"]["MinDistance"];
+
+	int i = 0;
+
+	auto MessagesConfig = Config["Messages"];
+	for (nlohmann::json::iterator it = MessagesConfig.begin(); it != MessagesConfig.end(); ++it)
+	{
+		Messages[i++] = ArkApi::Tools::Utf8Decode(*it);
+	}
 }
