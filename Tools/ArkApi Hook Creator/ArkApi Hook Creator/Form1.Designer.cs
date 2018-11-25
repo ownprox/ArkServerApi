d96a73a7cf180c2ec76cf6e1c115ca9880965111
OwnProx <ownproxie@outlook.com>
2018-11-25 08:43:23 +0000

    ArkApi Hook Generator

---
+++ Tools/ArkApi Hook Creator/ArkApi Hook Creator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ArkApi_Hook_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StructCombo = new System.Windows.Forms.ComboBox();
+            this.FuncCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ClassCombo = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // StructCombo
+            // 
+            this.StructCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.StructCombo.FormattingEnabled = true;
+            this.StructCombo.Location = new System.Drawing.Point(466, 6);
+            this.StructCombo.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.StructCombo.Name = "StructCombo";
+            this.StructCombo.Size = new System.Drawing.Size(415, 31);
+            this.StructCombo.TabIndex = 1;
+            this.StructCombo.SelectedIndexChanged += new System.EventHandler(this.StructCombo_SelectedIndexChanged);
+            // 
+            // FuncCombo
+            // 
+            this.FuncCombo.Enabled = false;
+            this.FuncCombo.FormattingEnabled = true;
+            this.FuncCombo.Location = new System.Drawing.Point(1024, 6);
+            this.FuncCombo.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.FuncCombo.Name = "FuncCombo";
+            this.FuncCombo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.FuncCombo.Size = new System.Drawing.Size(569, 31);
+            this.FuncCombo.TabIndex = 2;
+            this.FuncCombo.SelectedIndexChanged += new System.EventHandler(this.FuncCombo_SelectedIndexChanged);
+            this.FuncCombo.TextUpdate += new System.EventHandler(this.FuncCombo_TextUpdate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(348, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Structure:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(912, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Function:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Class:";
+            // 
+            // ClassCombo
+            // 
+            this.ClassCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ClassCombo.FormattingEnabled = true;
+            this.ClassCombo.Location = new System.Drawing.Point(90, 6);
+            this.ClassCombo.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.ClassCombo.Name = "ClassCombo";
+            this.ClassCombo.Size = new System.Drawing.Size(238, 31);
+            this.ClassCombo.TabIndex = 0;
+            this.ClassCombo.SelectedIndexChanged += new System.EventHandler(this.ClassCombo_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1608, 606);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1608, 652);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ClassCombo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FuncCombo);
+            this.Controls.Add(this.StructCombo);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "ArkApi Hook Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox StructCombo;
+        private System.Windows.Forms.ComboBox FuncCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ClassCombo;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Tools/ArkApi Hook Creator/ArkApi Hook Creator/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ArkApi_Hook_Creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<int, Dictionary<string, Dictionary<int, string>>> FunctionInfo = new Dictionary<int, Dictionary<string, Dictionary<int, string>>>();
+        Dictionary<string, Dictionary<int, string>> StructureSelector;
+        Dictionary<int, string> FunctionSelector;
+
+        private void AddFunction(int ClassIndex, string Structure, int FunctionIndex, string Function)
+        {
+            if (FunctionInfo.TryGetValue(ClassIndex, out StructureSelector))
+            {
+                if (StructureSelector.TryGetValue(Structure, out FunctionSelector))
+                {
+                    FunctionSelector.Add(FunctionIndex, Function);
+                }
+                else
+                {
+                    Dictionary<int, string> NewFuncData = new Dictionary<int, string>();
+                    NewFuncData.Add(FunctionIndex, Function);
+                    StructureSelector.Add(Structure, NewFuncData);
+                }
+            }
+            else
+            {
+                Dictionary<string, Dictionary<int, string>> NewStructData = new Dictionary<string, Dictionary<int, string>>();
+                Dictionary<int, string> NewFuncData = new Dictionary<int, string>();
+                NewFuncData.Add(FunctionIndex, Function);
+                NewStructData.Add(Structure, NewFuncData);
+                FunctionInfo.Add(ClassIndex, NewStructData);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClassCombo.Items.AddRange(new string[] { "Actor", "GameMode", "GameState", "Inventory", "Other", "PrimalStructure", "Tribe" });
+
+            int ClassIndex = 0;
+            foreach (string ArkHeader in ClassCombo.Items)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    int ClassId = ClassIndex++;
+                    wc.DownloadStringCompleted += (object s, DownloadStringCompletedEventArgs ea) => ParseArkHeader(ClassId, s, ea);
+                    wc.DownloadStringAsync(new Uri("https://raw.githubusercontent.com/Michidu/ARK-Server-API/master/version/Core/Public/API/ARK/" + ArkHeader + ".h"));
+                }
+            }
+        }
+
+        private void ParseArkHeader(int ClassIndex, object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Error: " + e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int FindIndex = -1, StructureIndex = 0, FunctionIndex = 0;
+            string StructName = "";
+            string[] splts = Regex.Split(e.Result, "struct ");
+            for (int i = 1; i < splts.Length; i++)
+            {
+                //Structure Name
+                FindIndex = splts[i].IndexOf("\n");
+                StructName = splts[i].Substring(0, FindIndex);
+                if ((FindIndex = StructName.IndexOf(" :")) != -1) StructName = StructName.Substring(0, FindIndex);
+
+                //Find Functions
+                if ((FindIndex = splts[i].IndexOf("// Functions")) != -1)
+                {
+                    FindIndex += 15;
+                    string[] splts2 = splts[i].Substring(FindIndex, splts[i].Length - FindIndex).Split('\n');
+                    foreach (string s in splts2)
+                    {
+                        if (s.Length > 5)
+                        {
+                            AddFunction(ClassIndex, StructName.Replace(" * ", "* "), FunctionIndex++, s.Replace("\t", ""));
+                        }
+                    }
+                    FunctionIndex = 0;
+                    StructureIndex++;
+                }
+            }
+        }
+
+        private void ClassCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StructCombo.Items.Clear();
+            if (FunctionInfo.TryGetValue(ClassCombo.SelectedIndex, out StructureSelector))
+            {
+                foreach (KeyValuePair<string, Dictionary<int, string>> StructShit in StructureSelector)
+                {
+                    StructCombo.Items.Add(StructShit.Key);
+                }
+            }
+        }
+
+        private void StructCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FuncCombo.Items.Clear();
+            FuncCombo.Enabled = true;
+            FuncCombo.Text = "";
+            if (FunctionInfo.TryGetValue(ClassCombo.SelectedIndex, out StructureSelector))
+            {
+                if (StructureSelector.TryGetValue(StructCombo.Text, out FunctionSelector))
+                {
+                    foreach (string FunctionName in FunctionSelector.Values)
+                    {
+                        if (FunctionName.Contains(" { ")) FuncCombo.Items.Add(Regex.Split(FunctionName, " { ")[0].Replace(" * ", "* "));
+                        else FuncCombo.Items.Add(FunctionName.Replace(" * ", "* "));
+                    }
+                }
+            }
+        }
+
+        private string LowerCase(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return str;
+            return Char.IsLower(str, 0) ? (Char.ToUpperInvariant(str[0]) + str.Substring(1)) : (Char.ToLowerInvariant(str[0]) + str.Substring(1));
+        }
+
+        private void FuncCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            if (FunctionInfo.TryGetValue(ClassCombo.SelectedIndex, out StructureSelector))
+            {
+                if (StructureSelector.TryGetValue(StructCombo.Text, out FunctionSelector))
+                {
+                    string FunctionData;
+                    if (FunctionSelector.TryGetValue(FuncCombo.SelectedIndex, out FunctionData))
+                    {
+                        if(FunctionData.Contains("NativeCall<"))
+                        {
+                            string FunctionVariables = Regex.Split(FunctionData, "NativeCall<")[1];
+                            FunctionVariables = FunctionVariables.Substring(0, FunctionVariables.IndexOf('>')).Replace(" * ", "* ");
+                            string FriendlyHookName = FuncCombo.Text;
+                            FriendlyHookName = FriendlyHookName.Replace("* ", "").Replace("()", "");
+                            if (FriendlyHookName.Contains(" ")) FriendlyHookName = FriendlyHookName.Split(' ')[1];
+                            if(FriendlyHookName.Contains("(")) FriendlyHookName = FriendlyHookName.Split('(')[0];
+                            string Hook = "DECLARE_HOOK(" + StructCombo.Text + "_" + FriendlyHookName;
+                            string HookFunc = "";
+                            if (FunctionVariables.Contains(", "))
+                            {
+                                string[] Vars = Regex.Split(FunctionVariables, ", ");
+                                foreach (string s in Vars) Hook += ", " + s;
+                                HookFunc = Vars[0] + " _cdecl ";
+                            }
+                            else
+                            {
+                                Hook += ", " + FunctionVariables;
+                                HookFunc = FunctionVariables + " _cdecl ";
+                            }
+                            Hook += ");";
+                            HookFunc += " Hook_" + StructCombo.Text + "_" + FriendlyHookName + "(" + StructCombo.Text + "* " + LowerCase(StructCombo.Text);
+                            string Variables = FuncCombo.Text;
+                            int FindIndex;
+                            if((FindIndex = Variables.IndexOf('(')) != -1)
+                            {
+                                Variables = Variables.Remove(0, FindIndex + 1);
+                                FindIndex = Variables.IndexOf(')');
+                                Variables = Variables.Substring(0, FindIndex + 1);
+                                HookFunc += (Variables.Length > 1 ? ", " + Variables : ")") + "\n{\n" + (HookFunc.StartsWith("void") ? "    " : "   return ");
+                                HookFunc += StructCombo.Text + "_" + FriendlyHookName + "_original(" + LowerCase(StructCombo.Text);
+                                if (Variables.Length > 1)
+                                {
+                                    string[] Vars = Regex.Split(Variables, ", ");
+                                    foreach (string s in Vars)
+                                    {
+                                       if (s.Contains(" ")) HookFunc += ", " + s.Split(' ')[1].Replace(")", "");
+                                    }
+                                    HookFunc += ");\n}";
+                                }
+                                else HookFunc += ");\n}";
+                                richTextBox1.AppendText(Hook + Environment.NewLine + Environment.NewLine + HookFunc + Environment.NewLine + Environment.NewLine
+                                    + "ArkApi::GetHooks().SetHook(\"" + StructCombo.Text + "." + FriendlyHookName + "\", &Hook_" + StructCombo.Text + "_" + FriendlyHookName + ", &" + StructCombo.Text + "_" + FriendlyHookName + "_original);" + Environment.NewLine + Environment.NewLine
+                                    + "ArkApi::GetHooks().DisableHook(\"" + StructCombo.Text + "." + FriendlyHookName + "\");");
+                                Clipboard.SetText(richTextBox1.Text);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FuncCombo_TextUpdate(object sender, EventArgs e)
+        {
+            if(ClassCombo.SelectedIndex == -1)
+            {
+                FuncCombo.Text = "";
+                MessageBox.Show("Please Select a Class First!", "Class Not Selected!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            if (StructCombo.SelectedIndex == -1)
+            {
+                FuncCombo.Text = "";
+                MessageBox.Show("Please Select a Structure First!", "Structure Not Selected!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            FuncCombo.Items.Clear();
+            if (FunctionInfo.TryGetValue(ClassCombo.SelectedIndex, out StructureSelector))
+            {
+                if (StructureSelector.TryGetValue(StructCombo.Text, out FunctionSelector))
+                {
+                    string FuncName;
+                    foreach (string FunctionName in FunctionSelector.Values)
+                    {
+                        if (FunctionName.Contains(" { "))
+                        {
+                            FuncName = Regex.Split(FunctionName, " { ")[0].Replace(" * ", "* ");
+                            if (FuncName.ToLower().Contains(FuncCombo.Text.ToLower()))
+                                FuncCombo.Items.Add(FuncName);
+                        }
+                        else
+                        {
+                            FuncName = FunctionName.Replace(" * ", "* ");
+                            if (FuncName.ToLower().Contains(FuncCombo.Text.ToLower()))
+                                FuncCombo.Items.Add(FuncName);
+                        }
+                    }
+                }
+            }
+            FuncCombo.SelectionStart = FuncCombo.Text.Length;
+            FuncCombo.SelectionLength = 0;
+        }
+    }
+}
