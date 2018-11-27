7b136edc436c20dba0afc3b7a5f10b48c1fafaa7
OwnProx <ownproxie@outlook.com>
2018-11-27 03:45:51 +0000

    Version Added

--- Tools/ArkApi Hook Creator/ArkApi Hook Creator/Form1.Designer.cs
+++ Tools/ArkApi Hook Creator/ArkApi Hook Creator/Form1.Designer.cs
@@ -133,7 +133,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "Form1";
-            this.Text = "ArkApi Hook Creator";
+            this.Text = "ArkApi Hook Creator v1.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
