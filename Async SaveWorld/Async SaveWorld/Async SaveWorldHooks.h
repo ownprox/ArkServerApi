5be0fc85e2425ba42bf9822cfdf17eaf379d98b4
OwnProx <ownproxie@outlook.com>
2018-12-08 01:55:13 +0000

    Async Save World

---
+++ Async SaveWorld/Async SaveWorld/Async SaveWorldHooks.h
@@ -0,0 +1,33 @@
+#pragma once
+DECLARE_HOOK(AShooterGameMode_SaveWorld, void, AShooterGameMode *);
+bool Saving = false;
+
+void InitHooks()
+{
+	ArkApi::GetHooks().SetHook("AShooterGameMode.SaveWorld", &Hook_AShooterGameMode_SaveWorld, &AShooterGameMode_SaveWorld_original);
+}
+
+void RemoveHooks()
+{
+	ArkApi::GetHooks().DisableHook("AShooterGameMode.SaveWorld", &Hook_AShooterGameMode_SaveWorld);
+}
+
+void Hook_AShooterGameMode_SaveWorld(AShooterGameMode* aShooterGameMode)
+{
+	if (Saving)
+	{
+		Log::GetLog()->warn("World Already Saving!");
+		return;
+	}
+	Saving = true;
+	std::thread([aShooterGameMode]()
+	{
+		__try
+		{
+			AShooterGameMode_SaveWorld_original(aShooterGameMode);
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{ Log::GetLog()->error("Error failed to save!"); }
+		Saving = false;
+	}).detach();
+}
